33a9fb594760ab4db546940a06930a68b783445b
unknown <wrouvre@ippon.fr>
2023-03-01 07:54:00 +0100

    - Mobile practice added - url link feature

--- docs/practices/mobile.js
+++ docs/practices/mobile.js
@@ -2,10 +2,10 @@
 const mobileRadar = {  
   // Quadrants
   quadrants: [
-    { name: "framework" },
-    { name: "mbaas" },
-    { name: "yo" },
-    { name: "to" },
+    { name: "Frameworks" },
+    { name: "Tools" },
+    { name: "Concepts" },
+    { name: "Tests" },
   ],
 
   // Rings : 0=ADOPT ; 1=TRIAL ; 2=ASSESS ; 3=HOLD
@@ -14,49 +14,271 @@ const mobileRadar = {
   entries: [
 
     // Quadrant 0
+
+    // Native
     {
       quadrant: 0,
-      ring: 2,
-      label: "DL1",
+      ring: 0,
+      label: "Android Kotlin",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "Android Compose",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "iOS Swift",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "iOS SwiftUI",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "Ionic",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "PWA",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: ".NET MAUI",
+      active: true,
+      moved: 0,
+    },
+
+    // CrossPlatform
+    
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "Flutter",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "React Native",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "KMM",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "NativeScript",
       active: true,
       moved: 0,
     },
     {
       quadrant: 0,
       ring: 2,
-      label: "DL2",
+      label: "Unity",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 2,
+      label: "Flutter Web",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 2,
+      label: "Rust",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 1,
+      label: "Hermes",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 0,
+      ring: 2,
+      label: "KMM Compose",
       active: true,
       moved: 0,
     },
 
     // Quadrant 1
+
+    // MBaaS
     {
       quadrant: 1,
-      ring: 2,
-      label: "DW1",
+      ring: 0,
+      label: "Firebase",
       active: true,
       moved: 0,
     },
     {
       quadrant: 1,
-      ring: 2,
-      label: "DW2",
+      ring: 0,
+      label: "Amplify",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Azure Mobile Apps",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Backendless",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "8base",
+      link: "https://www.8base.com/",
+      active: true,
+      moved: 0,
+    },
+
+    // Ops
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "Bitrise",
+      link: "https://www.gg.com/",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "Buddy",
+      link: "https://www.gg.com/",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "GitlabCI",
+      link: "https://www.gg.com/",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "MDM",
+      link: "https://www.gg.com/",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Android Enterprise",
+      link: "https://www.gg.com/",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "iOS Busines",
+      link: "https://www.gg.com/",
       active: true,
       moved: 0,
     },
 
     // Quadrant 2
+
+    // Concepts
+    
     {
       quadrant: 2,
-      ring: 2,
-      label: "DP1",
+      ring: 0,
+      label: "Feature Flipping",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "ABTesting",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "Canary",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "Feature Flagging",
       active: true,
       moved: 0,
     },
     {
       quadrant: 2,
       ring: 2,
-      label: "DP2",
+      label: "Server Driven UI",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "Device Farming",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "PassKey",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "Super Apps",
       active: true,
       moved: 0,
     },
@@ -64,31 +286,59 @@ const mobileRadar = {
     // Quadrant 3
     {
       quadrant: 3,
-      ring: 2,
-      label: "ST1",
+      ring: 0,
+      label: "Espresso",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "JUnit",
       active: true,
       moved: 0,
     },
     {
       quadrant: 3,
       ring: 2,
-      label: "ST2",
+      label: "Accessibility",
       active: true,
       moved: 0,
     },
     {
       quadrant: 3,
       ring: 2,
-      label: "CP1",
+      label: "React Testing Library",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "XCodeUI Test",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "Jest",
+      active: true,
+      moved: 0,
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "Appium",
       active: true,
       moved: 0,
     },
     {
       quadrant: 3,
       ring: 2,
-      label: "CP2",
-      active: true,
-      moved: 0,
+      label: "Snapshot Testing",
+      active: false,
+      moved: -20,
     },
   ]
 }
